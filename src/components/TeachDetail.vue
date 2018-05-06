5fd2248e32cc11a39408685816fe12230b2be7f9
3wot <j1j2w3@163.com>
2018-05-06 21:01:16 +0800

    Signed-off-by: 3wot <j1j2w3@163.com>

--- src/components/TeachDetail.vue
+++ src/components/TeachDetail.vue
@@ -13,7 +13,7 @@
 
 
                 <div class="panel-t-title">
-                  <router-link to="/" slot="left">
+                  <!-- <router-link to="/" slot="left"> -->
                       <span style="line-height: 25px;
                           display: inline-block;
                           background-color: rgba(255, 255, 255, 0.701961);
@@ -22,8 +22,8 @@
                           height: 25px;
                           padding: 4px;
                           text-align: center;
-                          margin-top: 5px;" class="mintui mintui-back"></span>
-                  </router-link>
+                          margin-top: 5px;" @click="goBack" class="mintui mintui-back"></span>
+                  <!-- </router-link> -->
                   <img src="../../static/star.png" style="margin:10px;float:right;width:25px;height:25px;">
                 </div>
               </div>
@@ -137,77 +137,81 @@ export default {
       $.post(getActivityInfo, param, function(data){
         callback(data);
       })
+    },
+    goBack() {
+      console.log('ddddddddd')
+      this.$router.go(-1)
     }
   },
   data () {
     return {
-      teachCourseList:[
-      	{
-          id:'001',
-      		title: "未来领袖成长营--城市生存大挑战",
-      		img:'./static/teach004.png',
-      		type:"practice",
-      		price:180,
-          for:'18以内',
-          introduce:'5元能做什么？也许是一包零食，或许是一瓶饮料，甚至就是一包烟的钱，今天我们将让你您的孩子拿着5元钱，完成一系列的城市生存挑战。',
-          provider:"服务商：早学网旗舰店",
-          address:"地址：北京市中国国家博物馆",
-          tip:"\n1.5元钱生存资金\n2.一个便携式背包\n3.一个收纳袋子\n4.一瓶矿泉水",
-          comments:[]
-      	},
-      	{
-          id:'002',
-      		title: "中国特种工艺制作非物质文化遗产--青铜器",
-      		img:'./static/teach005.png',
-      		type:"culture",
-      		price:80,
-          for:'18以内',
-          introduce:'参观青铜器的制作过程，学习了解古代劳动人命的智慧。通过这里对我国悠久的历史，深厚的文化底蕴形成认知。',
-          provider:"服务商：早学网旗舰店",
-          address:"地址：山西省太原市省博物馆",
-          tip:"\n1.一个便携式背包\n2.一瓶矿泉水",
-          comments:[]
-      	},
-      	{
-          id:'003',
-      		title: "海洋馆一日参观",
-      		img:'./static/teach006.png',
-      		type:"science",
-      		price:280,
-          for:'18以内',
-          introduce:'北京市海洋馆是以展示七大海洋生物为主。集科普，观赏，旅游，娱乐于一体的爱国主义教育基地。',
-          provider:"服务商：早学网旗舰店",
-          address:"地址：北京市动物园",
-          tip:"\n1.一个便携式背包\n2.一个收纳袋子\n3.一瓶矿泉水",
-          comments:[]
-      	},
-      	{
-          id:'004',
-      		title: "舌尖上的美味--蔬菜的故事",
-      		img:'./static/teach008.png',
-      		type:"science",
-      		price:598,
-          for:'18以内',
-          introduce:'舌尖上的中国，介绍了我国大江南北各地的美食，你知道吗，美食背后是一种种颜色种类各异的蔬菜，它们又有怎样的故事呢？',
-          provider:"服务商：早学网旗舰店",
-          address:"地址：北京市植物园",
-          tip:"\n1.一个便携式背包\n2.一个收纳袋子\n3.一瓶矿泉水",
-          comments:[]
-      	},
-      	{
-          id:'005',
-      		title: "儿童舞蹈夏令营",
-      		img:'./static/teach007.png',
-      		type:"practice",
-      		price:480,
-          for:'18以内',
-          introduce:'炎炎夏日，您还在为孩子报各种补习班吗？累了大人，苦了孩子，何不让孩子试试舞蹈呢！',
-          provider:"服务商：早学网旗舰店",
-          address:"地址：北京市海淀区北京邮电大学",
-          tip:"\n1.一个便携式背包\n2.一个收纳袋子",
-          comments:[]
-      	}
-      ],
+      // teachCourseList:[
+      // 	{
+      //     id:'001',
+      // 		title: "未来领袖成长营--城市生存大挑战",
+      // 		img:'./static/teach004.png',
+      // 		type:"practice",
+      // 		price:180,
+      //     for:'18以内',
+      //     introduce:'5元能做什么？也许是一包零食，或许是一瓶饮料，甚至就是一包烟的钱，今天我们将让你您的孩子拿着5元钱，完成一系列的城市生存挑战。',
+      //     provider:"服务商：早学网旗舰店",
+      //     address:"地址：北京市中国国家博物馆",
+      //     tip:"\n1.5元钱生存资金\n2.一个便携式背包\n3.一个收纳袋子\n4.一瓶矿泉水",
+      //     comments:[]
+      // 	},
+      // 	{
+      //     id:'002',
+      // 		title: "中国特种工艺制作非物质文化遗产--青铜器",
+      // 		img:'./static/teach005.png',
+      // 		type:"culture",
+      // 		price:80,
+      //     for:'18以内',
+      //     introduce:'参观青铜器的制作过程，学习了解古代劳动人命的智慧。通过这里对我国悠久的历史，深厚的文化底蕴形成认知。',
+      //     provider:"服务商：早学网旗舰店",
+      //     address:"地址：山西省太原市省博物馆",
+      //     tip:"\n1.一个便携式背包\n2.一瓶矿泉水",
+      //     comments:[]
+      // 	},
+      // 	{
+      //     id:'003',
+      // 		title: "海洋馆一日参观",
+      // 		img:'./static/teach006.png',
+      // 		type:"science",
+      // 		price:280,
+      //     for:'18以内',
+      //     introduce:'北京市海洋馆是以展示七大海洋生物为主。集科普，观赏，旅游，娱乐于一体的爱国主义教育基地。',
+      //     provider:"服务商：早学网旗舰店",
+      //     address:"地址：北京市动物园",
+      //     tip:"\n1.一个便携式背包\n2.一个收纳袋子\n3.一瓶矿泉水",
+      //     comments:[]
+      // 	},
+      // 	{
+      //     id:'004',
+      // 		title: "舌尖上的美味--蔬菜的故事",
+      // 		img:'./static/teach008.png',
+      // 		type:"science",
+      // 		price:598,
+      //     for:'18以内',
+      //     introduce:'舌尖上的中国，介绍了我国大江南北各地的美食，你知道吗，美食背后是一种种颜色种类各异的蔬菜，它们又有怎样的故事呢？',
+      //     provider:"服务商：早学网旗舰店",
+      //     address:"地址：北京市植物园",
+      //     tip:"\n1.一个便携式背包\n2.一个收纳袋子\n3.一瓶矿泉水",
+      //     comments:[]
+      // 	},
+      // 	{
+      //     id:'005',
+      // 		title: "儿童舞蹈夏令营",
+      // 		img:'./static/teach007.png',
+      // 		type:"practice",
+      // 		price:480,
+      //     for:'18以内',
+      //     introduce:'炎炎夏日，您还在为孩子报各种补习班吗？累了大人，苦了孩子，何不让孩子试试舞蹈呢！',
+      //     provider:"服务商：早学网旗舰店",
+      //     address:"地址：北京市海淀区北京邮电大学",
+      //     tip:"\n1.一个便携式背包\n2.一个收纳袋子",
+      //     comments:[]
+      // 	}
+      // ],
       item:{},
       selected:'1',
     }
