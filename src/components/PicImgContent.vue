211b9958c73084f9747cd3651d548c599c6c3006
jjw <j1j2w3@163.com>
2018-06-04 23:51:06 +0800

    Signed-off-by: jjw <j1j2w3@163.com>

--- src/components/PicImgContent.vue
+++ src/components/PicImgContent.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="pic-img-content">
-      <div v-for="item in pics" v-if="pics.length>1" class="img-outer">
+      <div v-for="(item,index) in pics" v-if="pics.length>1" class="img-outer">
         <div class="img-iner">
-          <img class="" :src="item+'?imageView2/1/w/148/h/148/q/75|imageslim'"/>
+          <img class="" @click="openImg(index)" :src="item+'?imageView2/1/w/148/h/148/q/75|imageslim'"/>
         </div>
       </div>
 
@@ -13,6 +13,15 @@
 
       <div style="clear:both;"></div>
 
+
+      <div class="full-img" v-if="hasClick">
+        <mt-swipe :auto="0" :default-index="defaultIndex">
+          <mt-swipe-item v-for="item in pics" :key="item.index">
+              <img @click="closeImg" class="full-img-in" :src="item+'?imageView2/1/w/360/h/640'">
+          </mt-swipe-item>
+        </mt-swipe>
+
+      </div>
   </div>
 </template>
 
@@ -22,16 +31,26 @@ export default {
 
   name: 'PicImgContent',
   props: ['pics'],
+  methods: {
+    openImg(idx) {
+      this.hasClick = true
+      this.defaultIndex = idx
+    },
+    closeImg() {
+      this.hasClick = false
+    }
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      hasClick: false,
+      defaultIndex: 0,
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 .pic-img-content{
   width: 100%;
   height: auto;
@@ -44,16 +63,35 @@ export default {
   margin-right: 1.3%;
   position: relative;
 }
-.img-outer .img-iner{
+.pic-img-content .img-outer .img-iner{
   position: absolute;
   left: 0px;
   top: 0px;
   width: 100%;
   height: 100%;
 }
-.img-iner img{
+.pic-img-content .img-iner img{
   height: 100%;
   width: 100%;
 }
-
+.pic-img-content .full-img {
+  position:fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  z-index: 99;
+  background-color: #000; 
+}
+.pic-img-content .full-img-in {
+  position:relative;
+  height: 100%;
+  width: 100%;
+}
+.pic-img-content .mint-swipe-indicator {
+  background: #333;
+}
+.pic-img-content .mint-swipe-indicator.is-active{
+  background: #ffffff;
+}
 </style>
